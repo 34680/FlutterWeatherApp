550518fb3487e856021733426fa1a7c362878e28
34680 <116350782+34680@users.noreply.github.com>
2023-05-15 20:37:21 +0200

    Add files via upload

---
+++ lib/api/api_key.dart
@@ -0,0 +1,1 @@
+const apiKey = "d9138185a62487c81ccc410a6b34606e";
---
+++ lib/widgets/header_widget.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:weather_app/controller/global_controller.dart';
+
+class HeaderWidget extends StatefulWidget {
+  const HeaderWidget({Key? key}) : super(key: key);
+
+  @override
+  State<HeaderWidget> createState() => _HeaderWidgetState();
+}
+
+class _HeaderWidgetState extends State<HeaderWidget> {
+  String city = "";
+  String dateTime = DateFormat("yMMMMd").format(DateTime.now());
+  final GlobalController globalController =
+      Get.put(GlobalController(), permanent: true);
+
+  @override
+  void initState() {
+    getAddress(globalController.getLatitude().value,
+        globalController.getLongitude().value);
+    super.initState();
+  }
+
+  getAddress(latitude, longitude) async {
+    List<Placemark> placeMark =
+        await placemarkFromCoordinates(latitude, longitude);
+
+    Placemark place = placeMark[0];
+    setState(() {
+      city = place.locality!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(left: 20, right: 20),
+          alignment: Alignment.topLeft,
+          child: Text(
+            city,
+            style: const TextStyle(fontSize: 35, height: 2),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+          alignment: Alignment.topLeft,
+          child: Text(
+            dateTime,
+            style:
+                TextStyle(fontSize: 14, color: Colors.grey[700], height: 1.5),
+          ),
+        ),
+      ],
+    );
+  }
+}
